58d161a1cedb52cc41bfdcd9ca2838ddec0dfe94
Erik Perkins <erik.andrew.perkins@gmail.com>
2016-12-18 13:56:52 -0800

    Classify MNIST digits

--- main.py
+++ main.py
@@ -1,18 +1,29 @@
 from flask import Flask, Response, send_file, jsonify
 from StringIO import StringIO
-import json
-
-from tensorflow.examples.tutorials.mnist import input_data
-from PIL import Image
-from pylab import gray
-from numpy import random, uint8
+from network.mnist import MnistNetwork, digit
 
 app = Flask(__name__)
-MNIST = input_data.read_data_sets("./MNIST_data/", one_hot = True)
 
 @app.route("/")
 def hello():
-  return "Hello, World!"
+  return "Hello, Flask!"
+
+@app.route("/mnist")
+def mnist():
+  network = MnistNetwork()
+  id, prediction = network.predict()
+  response = """
+    <p>The neural network thinks <img src="image/%s"/> is %s.</p>
+  """ % (id, prediction)
+  return response
+
+@app.route("/image/<int:id>", methods = ["GET"])
+def getImage(id):
+  image = digit(id)
+  io = StringIO()
+  image.save(io, 'PNG')
+  io.seek(0)
+  return send_file(io, mimetype = "image/png")
 
 @app.route("/names/<string:name>")
 def getName(name):
@@ -21,18 +32,6 @@ def getName(name):
   response.status_code = 200
   response.headers['Link'] = 'http://www.backpasture.net'
   return response
-
-@app.route("/image", methods = ["GET"])
-def getImage():
-  id = random.randint(0, 10000)
-  record = MNIST.test.images[id]
-  array = record.reshape((28, 28))
-  image = Image.fromarray(uint8(255 * (1.0 - array)))
-
-  io = StringIO()
-  image.save(io, 'PNG')
-  io.seek(0)
-  return send_file(io, mimetype = "image/png")
 
 if __name__ == "__main__":
   app.run(host = "0.0.0.0", port = 3005, debug = True)
