de648f8ced003986e4f7f2faf8128a14514c5ae9
Erik Perkins <erik.andrew.perkins@gmail.com>
2016-12-24 05:04:06 -0800

    Accept base 64 encoded images; transform, classify, and return the result

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-from flask import Flask, Response, send_file, jsonify
+from flask import Flask, Response, send_file, jsonify, request
 from StringIO import StringIO
-from network.mnist import MnistNetwork, digit
+from network.mnist import MnistNetwork, digit, mnist_array, mnist_input
+
+
+import json
+from PIL import Image
 
 import base64
 import cStringIO
@@ -23,13 +27,42 @@ def mnist():
 @app.route("/mnist/<int:id>.json", methods = ["GET"])
 def getMnistJson(id):
   network = MnistNetwork()
-  id, prediction = network.predict(id)
+
+  input = mnist_input(id)
+  id, prediction = network.predict(input)
+
+  image = digit(id)
+  buffer = cStringIO.StringIO()
+  image.save(buffer, format="PNG")
+  encoded_image = base64.b64encode(buffer.getvalue())
+
   data = {
     'id': id,
     'prediction': prediction,
     'url': "/mnist/image/%s" % id,
+    'image': encoded_image
   }
   return jsonify(data)
+
+#######################
+
+@app.route("/mnist/classify.json", methods = ["GET"])
+def postMnistJson():
+  network = MnistNetwork()
+
+  image_json = request.args.get('image')
+  image_ascii = json.dumps(image_json).encode('ascii')
+  blob = base64.urlsafe_b64decode(image_ascii)
+
+  input = mnist_array(blob)
+  id, result = network.predict(input)
+
+  data = {
+    "result": result
+  }
+  return jsonify(data)
+
+#########################
 
 @app.route("/mnist/image/<int:id>.json")
 def getMnistImageJson(id):
--- network/mnist.py
+++ network/mnist.py
@@ -1,7 +1,9 @@
 import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
 from PIL import Image
-from numpy import random, expand_dims, uint8
+from numpy import random, expand_dims, uint8, array, shape
+
+from StringIO import StringIO
 
 MNIST = input_data.read_data_sets("./MNIST_data/", one_hot = True)
 
@@ -9,6 +11,19 @@ def digit(id):
   record = MNIST.test.images[id]
   array = record.reshape((28, 28))
   return Image.fromarray(uint8(255 * (1.0 - array)))
+
+# Maybe consolidate these two functions? Both return an MNIST input vector
+def mnist_array(blob):
+  bytes = bytearray(blob)
+  image = Image.open(StringIO(bytes))
+  image.thumbnail((28, 28), Image.ANTIALIAS)
+  image.convert("L")
+  image_array = array(image)[:,:,3] / 255.0
+  return image_array.reshape((784,))
+
+def mnist_input(id):
+  return MNIST.test.images[id]
+
 
 class MnistNetwork:
   def __init__(self):
@@ -88,11 +103,12 @@ class MnistNetwork:
 
     self.saver = tf.train.Saver()
 
-  def predict(self, id = None):
+  def predict(self, input):
     with tf.Session() as session:
       self.saver.restore(session, './tutorial-variables.ckpt')
-      id = id or random.randint(0, 10000)
-      datum = expand_dims(MNIST.test.images[id], axis = 0)
+      # id = id or random.randint(0, 10000)
+      # datum = expand_dims(MNIST.test.images[id], axis = 0)
+      datum = expand_dims(input, axis = 0)
       output = session.run(
         self.predict_operation,
         feed_dict = { self.x: datum, self.PROB: 1.0 }
